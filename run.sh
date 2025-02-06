35e40ff924aceae7d0dd60076e6856a973b29cba
Konstantinos Vytiniotis <konst.vyti@hotmail.com>
2025-02-07 00:55:18 +0200

    Adjust docker execution script

--- run.sh
+++ run.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-HASH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
-GPU=$1
-name=${USER}_pymarl_GPU_${GPU}_${HASH}
+readonly GPU=0
+readonly HASH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
+readonly NAME=${USER}_pymarl_GPU_${GPU}_${HASH}
 
 echo "Launching container named '${name}' on GPU '${GPU}'"
+
+# TODO: Add argument `--runtime=nvidia` to `docker run`
+#       once Dockerfile image is updated to Cuda v.12.8
+
 # Launches a docker container using our image, and runs the provided command
-
-if hash nvidia-docker 2>/dev/null; then
-  cmd=nvidia-docker
-else
-  cmd=docker
-fi
-
-NV_GPU="$GPU" ${cmd} run \
-    --name $name \
-    --user $(id -u):$(id -g) \
-    -v `pwd`:/pymarl \
-    -t pymarl:1.0 \
-    ${@:2}
+NV_GPU="$GPU" docker run \
+  --name $NAME \
+  --user $(id -u):$(id -g) \
+  -v `pwd`:/pymarl \
+  -t pymarl:1.0 \
+  ${@:1}
