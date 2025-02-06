d9ce724293e8fdf4db4cc88b2684899811507ad1
Konstantinos Vytiniotis <konst.vyti@hotmail.com>
2025-02-07 00:17:32 +0200

    Better SC2 installation script

--- install_sc2.sh
+++ install_sc2.sh
@@ -1,35 +1,45 @@
 #!/bin/bash
+
+# Bash Colors
+declare -A colors
+
+colors[light_blue]="\e[94m"
+colors[light_cyan]="\e[96m"
+colors[light_green]="\e[92m"
+colors[light_red]="\e[91m"
+colors[reset]="\e[0m"
+
+hello_world() {
+    echo -e "${colors[light_blue]}************************${colors[reset]}"
+    echo -e "${colors[light_cyan]}Have a great $(date +'%A')!${colors[reset]}"
+    echo -e "${colors[light_blue]}************************${colors[reset]}"
+}
+
+hello_world
+
+echo -e "${colors[light_green]}Setting up directories...${colors[reset]}"
+readonly CUR_DIR=$(cd $(dirname "$0") && pwd)
+readonly THIRD_PARTY_DIR="${CUR_DIR}/3rdparty/"
+readonly SC2PATH="${THIRD_PARTY_DIR}/StarCraftII/"
+readonly MAP_DIR="${SC2PATH}/Maps/"
+
+echo -e "${colors[light_green]}Creating 3rdparty directory...${colors[reset]}"
+mkdir -p $THIRD_PARTY_DIR && cd $THIRD_PARTY_DIR
+
 # Install SC2 and add the custom maps
-
-if [ -z "$EXP_DIR" ]
-then
-    EXP_DIR=~
-fi
-
-echo "EXP_DIR: $EXP_DIR"
-cd $EXP_DIR/pymarl
-
-mkdir 3rdparty
-cd 3rdparty
-
-export SC2PATH=`pwd`'/StarCraftII'
-echo 'SC2PATH is set to '$SC2PATH
-
 if [ ! -d $SC2PATH ]; then
-        echo 'StarCraftII is not installed. Installing now ...';
-        wget http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.10.zip
-        unzip -P iagreetotheeula SC2.4.10.zip
-        rm -rf SC2.4.10.zip
+    echo "StarCraftII is not installed. Installing now ..."
+    wget http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.10.zip
+    unzip -P iagreetotheeula SC2.4.10.zip
+    rm -rf SC2.4.10.zip
 else
-        echo 'StarCraftII is already installed.'
+    echo 'StarCraftII is already installed.'
 fi
 
-echo 'Adding SMAC maps.'
-MAP_DIR="$SC2PATH/Maps/"
-echo 'MAP_DIR is set to '$MAP_DIR
+echo "Adding SMAC maps. MAP_DIR is set to ${MAP_DIR}"
 
 if [ ! -d $MAP_DIR ]; then
-        mkdir -p $MAP_DIR
+    mkdir -p $MAP_DIR
 fi
 
 cd ..
@@ -39,4 +49,3 @@ mv SMAC_Maps $MAP_DIR
 rm -rf SMAC_Maps.zip
 
 echo 'StarCraft II and SMAC are installed.'
-
